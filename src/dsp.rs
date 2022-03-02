361fabd2bba20988e277c351b7ec0332a55bf254
DatDonathan <jojo08082003@gmail.com>
2022-03-02 08:06:15 +0100

    Using oscillator

--- src/dsp.rs
+++ src/dsp.rs
@@ -1,55 +1,54 @@
-pub trait Synthesizable {
-    fn synthesize(phase: f32) -> f32;
-}
-
 pub enum WaveForm {
     Sine,
     Saw,
     Square,
 }
 
-impl Synthesizable for WaveForm::Sine {
-    fn synthesize(phase: f32) {
-        return (phase * std::f64::consts::PI as f32).sin();
-    }
-}
-
-impl Synthesizable for WaveForm::Saw {
-    fn synthesize(phase: f32) {
-        return phase * 2.0 - 1.0;
-    }
-}
-
-impl Synthesizable for WaveForm::Square {
-    fn synthesize(phase: f32) {
-        if (phase < 0.5) {
-            return 1.0;
+impl WaveForm {
+    fn synthesize(&self, phase: f32) -> f32{
+        let f: f32 = 0.0;
+        match self {
+            WaveForm::Sine => f = (phase * std::f64::consts::PI as f32).sin(),
+            WaveForm::Square => {
+                if phase < 0.5 {
+                    f = 1.0;
+                }
+                else {
+                    f = -1.0;
+                }   
+            },
+            WaveForm::Saw => f = phase * 2.0 - 1.0,
         }
-        else {
-            return -1.0;
-        }
+        return f;
     }
 }
 
 pub struct Oscillator {
-    pub waveform: WaveForm = Sine;
-    pub freq: f32 = 220.0;
-
-    phase: f32 = 0;
+    pub waveform: WaveForm,
+    pub freq: f32,
+    phase: f32,
 }
 
 impl Oscillator {
 
-    fn process(&self, time_step: f32) {
-        self.phase += time_step * freq;
+    pub fn new() -> Oscillator {
+        return Oscillator {
+            waveform: WaveForm::Sine,
+            freq: 220.0,
+            phase: 0.0,
+        }
+    }
+
+    pub fn process(&mut self, time_step: f32) {
+        self.phase += time_step * self.freq;
         //Modulo
         while self.phase >= 1.0 {
             self.phase -= 1.0;
         }
     }
 
-    fn synthesize(&self) -> f32{
-        return waveform.synthesize(self.phase);
+    pub fn synthesize(&self) -> f32{
+        return self.waveform.synthesize(self.phase);
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
 use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
 use cpal::Sample;
-use math::round;
-
-fn saw_wave(freq: f32, time: f32) -> f32 {
-    let a = time * freq;
-    let prog: f32 = a - (round::floor(a as f64, 0) as f32);
-    return prog * 2.0 - 1.0;
-}
-
-fn square_wave(freq: f32, time: f32) -> f32 {
-    let a = time * freq;
-    let prog: f32 = a - (round::floor(a as f64, 0) as f32);
-    if (prog < 0.5) {
-        return 1.0;
-    }
-    else {
-        return -1.0;
-    }
-}
-
-fn sine_wave(freq: f32, time: f32) -> f32 {
-    let a = time * freq * std::f64::consts::PI as f32;
-    return a.sin();
-}
-
-fn process(time: f32) -> f32 {
-    return sine_wave(220.0, time) * 0.2;
-}
+mod dsp;
 
 fn main() {
     // Create audio pipeline
@@ -42,15 +16,15 @@ fn main() {
     let range = device.supported_output_configs().expect("Error!").next().expect("No config found!");
     println!("{} / {}", range.min_sample_rate().0, range.max_sample_rate().0);
     let config = range.with_sample_rate(cpal::SampleRate(sample_rate)).config();
-    let mut time: f32 = 0.0;
     let time_step: f32 = 1.0/(sample_rate as f32);
+    let osc = dsp::Oscillator::new();
 
     let stream = device.build_output_stream(
         &config,
         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
             for sample in data.iter_mut() {
-                *sample = Sample::from(&process(time));
-                time += time_step;
+                osc.process(time_step);
+                *sample = Sample::from(&osc.synthesize());
             }
         },
         move |_err| {
