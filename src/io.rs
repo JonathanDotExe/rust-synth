65b8b1f312daa50f7536909ee5c54802adda8e28
DatDonathan <jojo08082003@gmail.com>
2022-03-08 13:45:29 +0100

    Fixed references in midi handler

--- src/io.rs
+++ src/io.rs
@@ -1,20 +1,51 @@
-use midir::{MidiInput, Ignore};
+use midir::{MidiInput, MidiInputConnection, Ignore};
+use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
+use cpal::Sample;
 
 pub struct AudioMidiHandler {
-    
+    _midiconn: MidiInputConnection<()>,
 }
 
 impl AudioMidiHandler {
 
     pub fn new() -> AudioMidiHandler {
-        return AudioMidiHandler {};
-    }
-
-    pub fn run(&mut self) {
         //Audio
-        //TODO
+        // Create audio pipeline
+        let host = cpal::default_host();
+        let device = host.default_output_device().expect("No device available!");
+        let sample_rate = 48000;
+        //TODO query configs
+        /*let config = cpal::StreamConfig{
+            channels: 2,
+            buffer_size: cpal::BufferSize::Fixed(256),
+            sample_rate: cpal::SampleRate(sample_rate),
+        };*/
+        let range = device.supported_output_configs().expect("Error!").next().expect("No config found!");
+        println!("{} / {}", range.min_sample_rate().0, range.max_sample_rate().0);
+        let channels = range.channels() as u128;
+        let config = range.with_sample_rate(cpal::SampleRate(sample_rate)).config();
+        let time_step: f32 = 1.0/(sample_rate as f32);
+        let mut sample_count: u128 = 0;
+        let mut s = 0.0;
 
-        //Midi-Input
+        let stream = device.build_output_stream(
+            &config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| { 
+                for sample in data.iter_mut() {
+                    if sample_count % channels == 0 {
+                        osc.process(time_step);
+                        s = osc.synthesize() * 0.2;
+                    }
+                    *sample = Sample::from(&s);
+                    sample_count += 1;
+                }
+            },
+            move |_err| {
+                println!("Error while running audio thread!")
+            },
+        ).unwrap();
+        stream.play().unwrap();
+        //MIDI
         let mut midiin = MidiInput::new("App-In").unwrap();
         midiin.ignore(Ignore::None);
         let ports = midiin.ports();
@@ -24,9 +55,13 @@ impl AudioMidiHandler {
         let port = &ports[0];
         println!("Using port {}!", midiin.port_name(&port).unwrap());
 
-        let _conn = &midiin.connect(port, "App-In", move |stamp, message, _| {
+        let midiconn = midiin.connect(port, "App-In", move |stamp, message, _| {
             println!("Midi Message {}: {:?} (len={}", stamp, message, message.len());
         }, ()).unwrap();
+
+        return AudioMidiHandler {
+            _midiconn: midiconn
+        };
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use cpal::traits::{HostTrait, DeviceTrait, StreamTrait};
-use cpal::Sample;
 
 mod dsp;
 mod voice;
@@ -8,46 +6,8 @@ mod midi;
 mod io;
 
 fn main() {
-    // Create audio pipeline
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("No device available!");
-    let sample_rate = 48000;
-    //TODO query configs
-    /*let config = cpal::StreamConfig{
-        channels: 2,
-        buffer_size: cpal::BufferSize::Fixed(256),
-        sample_rate: cpal::SampleRate(sample_rate),
-    };*/
-    let range = device.supported_output_configs().expect("Error!").next().expect("No config found!");
-    println!("{} / {}", range.min_sample_rate().0, range.max_sample_rate().0);
-    let channels = range.channels() as u128;
-    let config = range.with_sample_rate(cpal::SampleRate(sample_rate)).config();
-    let time_step: f32 = 1.0/(sample_rate as f32);
-    let mut osc: dsp::Oscillator = dsp::Oscillator::new();
-    let mut sample_count: u128 = 0;
-    let mut s = 0.0;
-
-    let stream = device.build_output_stream(
-        &config,
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| { 
-            for sample in data.iter_mut() {
-                if sample_count % channels == 0 {
-                    osc.process(time_step);
-                    s = osc.synthesize() * 0.2;
-                }
-                *sample = Sample::from(&s);
-                sample_count += 1;
-            }
-        },
-        move |_err| {
-            println!("Error while running audio thread!")
-        },
-    ).unwrap();
-    stream.play().unwrap();
-
     //Audio
-    let mut handler = io::AudioMidiHandler::new();
-    handler.run();
+    let mut _handler = io::AudioMidiHandler::new();
 
     //Wait
     println!("Press ENTER to stop!");
